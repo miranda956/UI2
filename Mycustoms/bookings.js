ad1cc6aeba9a65a1d937754721c790e4092bd3ef
EasyCoder1 <maina.alex2010@gmail.com>
2021-05-27 15:03:56 +0300

    modified booking

--- Mycustoms/bookings.js
+++ Mycustoms/bookings.js
@@ -1,48 +1,51 @@
-// bookings by provider id 
+$("document").ready(() => {
+    loadbookings();
+	
+});
+
 
 var loadbookings =()=>{
+    
     var url="http://172.105.167.182:8081/services/booking/all"
     var token = window.sessionStorage.getItem("token");
+    var html=``;
     // console.log(token)
-    if(token!==""){
         fetch(url, {
             method:"GET",
             headers: {
                 'Content-Type': 'application/json',
-               
-                'Authorization': 'Bearer ' +token,
-                
-              },  
-        }).then(res => {
-                if(res.ok){
+                'Authorization': 'Bearer ' +token,  
+              } 
+        })
+        .then(res=>{
+            if(res.ok){
                 return res.json();
             }
             else{
-                throw new Error("A server error occurred")
+                throw new Error("Error Occurred")
             }
-        }).then((data)=>{
-            var html="";
-            data.forEach((provider)=>{
-               html+=`<tr>  
-               <td>${provider.firstName} ${provider.secondName}</td>
-               <td>${provider.phone}</td>
-               <td>${provider.email}</td>
-               <td>${provider.blocked}</td>
-               <td nowrap="nowrap">
-                             <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mx-5" title="Edit details">\
-								<i class="la la-edit"></i>\
-							</a>\
-							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
-								<i class="la la-trash"></i>\
-							</a>\
-               </td>
-               </tr> `
-            })
-
-        document.getElementById("provider_table").innerHTML=html
+        })
+        .then((res)=>{ 
+           res.forEach(b => {
+            html+=`<tr>
+            <td>${b.userId} </td>
+            <td>${new Date(b.date)}</td>
+            <td>${b.hour}</td>
+            <td>${b.cancelled? `Active`:`Cancelled`}</td>
+            <td nowrap="nowrap">
+                              <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mx-5" title="Edit details">\
+                                 <i class="la la-edit"></i>\
+                             </a>\
+                             <a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+                                 <i class="la la-trash"></i>\
+                             </a>\
+             </td>
+            </tr>`
+               
+           });
+            document.getElementById("bookings_table").innerHTML=html;
         })
         .catch(err=>{
-            // console.log(err);
+            console.log(err);
         })
-    }
 }
